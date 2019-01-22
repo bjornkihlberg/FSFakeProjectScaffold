b43d373148f7443e819b2f20c5909d5a19bba9b4
Björn Kihlberg <bjorn.kihlberg@frontedgeit.se>
2019-01-22 15:23:20 +0100

    cleaning script

--- build.fsx
+++ build.fsx
@@ -3,3 +3,16 @@ nuget Fake.IO.FileSystem
 nuget Fake.DotNet.Cli
 nuget Fake.Core.Target //"
 #load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+let clean _ =
+    !! "./**/bin"
+    ++ "./**/obj"
+    |> Shell.cleanDirs
+
+Target.create "Clean" clean
