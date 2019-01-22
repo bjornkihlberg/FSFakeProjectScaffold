724558efaff53f1029ad37d24fcf0ac944ff3c78
Björn Kihlberg <bjorn.kihlberg@frontedgeit.se>
2019-01-22 15:35:20 +0100

    xunit testing classlib

---
+++ LibTests/LibraryTests.fs
@@ -0,0 +1,14 @@
+﻿namespace LibTests
+
+open Lib.Library
+
+open Xunit
+
+module LibraryTests =
+    [<Fact>]
+    let ``f test1`` () =
+        Assert.True (f 4 = 8)
+    
+    [<Fact>]
+    let ``f test2`` () =
+        Assert.Equal (10, f 5)
